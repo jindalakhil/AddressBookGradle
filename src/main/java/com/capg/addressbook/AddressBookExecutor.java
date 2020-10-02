60a506634382d7a0056b2d66dcc6b43ad56c90aa
jindalakhil <akhiljindal222@gmail.com>
2020-10-02 15:34:44 +0530

    [Akhil]Added: Master initial commit

---
+++ src/main/java/com/capg/addressbook/AddressBookExecutor.java
@@ -0,0 +1,11 @@
+package com.capg.addressbook;
+
+public class AddressBookExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Address Book Program");
+	}
+}
+
+
